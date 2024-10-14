c782a164c283074fb9b0ee0742249579c77bbd9b
josephat-mande <jassymande19@gmail.com>
2024-10-14 22:45:56 +0300

    add intoduction function that introduces the user when called upon

---
+++ intro.py
@@ -0,0 +1,11 @@
+class person:
+    def __init__(self, name, age, gender):
+        self.name = Joe
+        self.age = 22
+        self.gender = Male
+
+        def intro(self):
+            print(f"Hello, my name is {self.name},{self.gender} and I am {self.age} years old.")
+
+            person1 = person("Joe", 22, "Male")
+            person1.intro()
